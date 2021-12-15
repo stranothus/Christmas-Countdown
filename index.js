c90c5ed927d0a61d213b17dcd69b89908baee637
stranothus <stranothus@gmail.com>
2021-12-15 14:52:21 -0600

    Express Server

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-import * as PImage from "pureimage";
-import * as fs from "fs";
-import { measureText } from "pureimage/src/text.js";
+import express from "express";
+import updateImage from "./updateImage.js";
 
-// make image
-const img = await PImage.decodeJPEGFromStream(fs.createReadStream("background.jpeg"));
+const app = express();
 
-// get canvas context
-const ctx = img.getContext("2d");
-await new Promise((resolve, reject) => PImage.registerFont("./BakbakOne-Regular.ttf", "Bakbak").load(resolve));
+app.use(async (req, res) => {
+    await updateImage();
 
-const time = (() => {
-    let timeNow = Date.now();
-    let nextChristmas = new Date("Dec 25 " + (new Date().getMonth() === 11 && new Date().getDay() >= 25 ? new Date().getFullYear() + 1 : new Date().getFullYear())).getTime();
-
-    let time = nextChristmas - timeNow;
-    let toReturn = "";
-
-    if(Math.floor(time / (1000 * 60 * 60 * 24))) {
-        toReturn += Math.floor(time / (1000 * 60 * 60 * 24)) + " Days ";
-        time -= Math.floor(time / (1000 * 60 * 60 * 24)) * 1000 * 60 * 60 * 24;
-    }
-    if(Math.floor(time / (1000 * 60 * 60))) {
-        toReturn += Math.floor(time / (1000 * 60 * 60)) + " Hours ";
-        time -= Math.floor(time / (1000 * 60 * 60)) * 1000 * 60 * 60;
-    }
-    if(Math.floor(time / (1000))) {
-        toReturn += Math.floor(time / (1000 * 60)) + " Minutes ";
-        time -= Math.floor(time / (1000 * 60)) * 1000 * 60;
-    }
-    if(Math.floor(time / (1000))) {
-        toReturn += Math.floor(time / (1000 )) + " Seconds ";
-        time -= Math.floor(time / (1000)) * 1000;
-    }
-
-    return toReturn;
-})();
-
-ctx.fillStyle = "black";
-ctx.font = "30px Bakbak";
-ctx.textAlign = "center";
-ctx.fillText(time, img.width / 2, 100);
-ctx.font = "75px Bakbak";
-ctx.fillText("Until Christmas", img.width / 2, 200);
-
-//write to "out.png"
-PImage.encodePNGToStream(img, fs.createWriteStream("out.png")).then(() => {
-    console.log("wrote out the png file to out.png");
-}).catch((e)=>{
-    console.log("there was an error writing");
+    res.sendFile(process.cwd() + "/out.png");
 });
+
+app.listen(3030);
